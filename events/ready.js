98b8bd17e0587e56f3e5a36c081badcb51a205e3
MrTurtles <mineshaftdave2@gmail.com>
2018-08-25 13:08:12 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 //const client = new Discord.Client();
 module.exports = client => {
   console.log(chalk.green('I\'m Online'));
-  let login = ',help | I Like Dora!'
+  let login = ';help | I Like Dora!'
   client.user.setGame(login, 'https://www.twitch.tv/dora');
   client.user.setStatus('online');
   console.log(chalk.green(login));
