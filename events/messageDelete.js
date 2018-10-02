a05fc45167eb4b7cb0bf28ac7399ef835ca86e74
MrTurtles <mineshaftdave2@gmail.com>
2018-10-02 13:44:04 +0200

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,14 +3,14 @@ module.exports = messageDelete => {
   let actionlog = messageDelete.guild.channels.find('name', 'action-log');
   if(messageDelete.author.bot) return;
 
-  console.log(messageDelete.content)
-  messageDelete.channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author.tag} was deleted.`)
+  //console.log(messageDelete.content)
+  //messageDelete.channel.send(`The message : "${messageDelete.content}" by ${messageDelete.author.tag} was deleted.`)
 
   const embed = new Discord.RichEmbed()
     .setColor(0x11B8D6)
     .setTimestamp()
     .setTitle('Message Deleted')
-    .addField('User', `${messageDelete.author.tag} (@<${messageDelete.author.id}>)`)
+    .addField('User', `${messageDelete.author.tag} (<@${messageDelete.author.id}>)`)
     .addField('Message', messageDelete.content);
 
   if (!actionlog) return messageDelete.author.sendMessage(`Er is geen #logs channel.\nAls je deze commando wilt loggen moet je deze channel aanmaken:'#logs'`);
