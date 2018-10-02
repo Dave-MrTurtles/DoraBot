284e14b1c8c18b3f363b9bdae73d1c6ca0c98c6b
MrTurtles <mineshaftdave2@gmail.com>
2018-10-02 13:38:15 +0200

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -13,7 +13,7 @@ module.exports = messageDelete => {
     .addField('User', `${messageDelete.author.tag} (@<${messageDelete.author.id}>)`)
     .addField('Message', messageDelete.content);
 
-  if (!actionlog) return message.author.sendMessage(`Er is geen #logs channel.\nAls je deze commando wilt loggen moet je deze channel aanmaken:'#logs'`);
-  message.guild.channels.get(actionlog.id).sendEmbed(embed);
+  if (!actionlog) return messageDelete.author.sendMessage(`Er is geen #logs channel.\nAls je deze commando wilt loggen moet je deze channel aanmaken:'#logs'`);
+  messageDelete.guild.channels.get(actionlog.id).sendEmbed(embed);
 
 };
