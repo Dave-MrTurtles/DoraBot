6b5a810630c357c8f347fbb32fc074e5b4a590cc
MrTurtles <mineshaftdave2@gmail.com>
2018-10-02 17:28:13 +0200

    Add files via upload

---
+++ events/messageUpdate.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+module.exports = "messageUpdate", (oldmsg, newmsg) => {
+  const embed = new Discord.RichEmbed()
+    .setColor(0x11B8D6)
+    .setTimestamp()
+    .setTitle('Message Edited')
+    .addField('User', `${messageUpdate.author.tag} (@<${messageUpdate.author.id}>)`)
+    .addField('Message', messageUpdate.content)
+    .addField('Message', messageUpdate.content);
+  let actionlog = messageUpdate.guild.channels.find('name', 'action-log');
+  if(messageUpdate.author.bot) return;
+  if(newmsg.content != oldmsg){
+       if (!actionlog) return messageUpdate.author.sendMessage(`Er is geen #logs channel.\nAls je deze commando wilt loggen moet je deze channel aanmaken:'#logs'`);
+       messageUpdate.guild.channels.get(actionlog.id).sendEmbed(embed);
+   }
+  console.log(messageUpdate.content)
+  //messageUpdate.channel.send(`The message : "${messageUpdate.content}" by ${messageUpdate.author.tag} was deleted.`)
+
+  /*const embed = new Discord.RichEmbed()
+    .setColor(0x11B8D6)
+    .setTimestamp()
+    .setTitle('Message Edited')
+    .addField('User', `${messageUpdate.author.tag} (@<${messageUpdate.author.id}>)`)
+    .addField('Message', messageUpdate.content)
+    .addField('Message', messageUpdate.content);
+
+  if (!actionlog) return messageUpdate.author.sendMessage(`Er is geen #logs channel.\nAls je deze commando wilt loggen moet je deze channel aanmaken:'#logs'`);
+  messageUpdate.guild.channels.get(actionlog.id).sendEmbed(embed);*/
+
+};
